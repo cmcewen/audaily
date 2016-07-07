3d5b2fd0f7beab2925f93b22d45a7a7a41c2e406
stephenmeszaros <contact.meszaros@gmail.com>
2016-07-07 16:03:15 -0600

    Static page controllers

--- index.ios.js
+++ index.ios.js
@@ -27,30 +27,32 @@ class audaily extends Component {
         <StatusBar
         barStyle="light-content"
         />
-        <View style={styles.header}>
-            <Text style={styles.title}>Audaily</Text>
-            <Image
-            source={require('./images/settingsIcon@2x.png')}
-            style={styles.settingsIcon}
-            />
-        </View>
-        <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        snapToInterval={card_width + card_margin*2}
-        decelerationRate={0}
-        snapToAlignment= 'start'
-        >
-            <View style={styles.card}>
-                <Text>Card 1</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>Audaily</Text>
+                <Image
+                source={require('./images/settingsIcon@2x.png')}
+                style={styles.settingsIcon}
+                />
             </View>
-            <View style={styles.card}>
-                <Text>Card 2</Text>
+            <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            snapToInterval={card_width + card_margin*2}
+            decelerationRate={0.2}
+            snapToAlignment= 'start'
+            >
+                <View style={styles.card}>
+                </View>
+                <View style={styles.card}>
+                </View>
+                <View style={styles.card}>
+                </View>
+            </ScrollView>
+            <View style={styles.scrollViewIndicators}>
+                <View style={styles.scrollViewIndicatorDots}></View>
+                <View style={[styles.scrollViewIndicatorDots, styles.opacity]}></View>
+                <View style={[styles.scrollViewIndicatorDots, styles.opacity]}></View>
             </View>
-            <View style={styles.card}>
-                <Text>Hi Daiji</Text>
-            </View>
-        </ScrollView>
         </View>
         );
     }
@@ -94,6 +96,22 @@ var styles = StyleSheet.create({
             width: 0
         },
         shadowRadius: 6,
+    },
+    scrollViewIndicators: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        bottom: 38
+    },
+    scrollViewIndicatorDots: {
+        width: 6,
+        height: 6,
+        borderRadius: 3,
+        marginHorizontal: 3,
+        backgroundColor: '#ffffff',
+        opacity: 1
+    },
+    opacity: {
+        opacity: 0.4
     },
 });
 
